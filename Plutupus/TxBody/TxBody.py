960735d18af499061d7a5668024236b1a06c48ce
Mateus Santos <santos.mateus@protonmail.com>
2022-04-16 11:07:15 -0300

    completed txbody tests

--- Plutupus/TxBody/TxBody.py
+++ Plutupus/TxBody/TxBody.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-import json
+import logging
 
-from typing import List, Dict
+from typing import Any
 
 from Plutupus.Types.TxOutRef import TxOutRef
 from Plutupus.Types.Value import Value
@@ -10,46 +10,50 @@ from Plutupus.Types.Value import Value
 
 class TxBody(object):
 
-    def __init__(self, era, magic):
-        self.era = era
-        self.magic = magic
-
-        self.setup()
-
-    def setup(self):
-        # utxo
+    def __init__(self):
+        # utxo {"hash": <txid>, "index": <txix>}
         self.collateral = None
 
-        # list of utxos
-        self.inputs = []
+        # list of utxos {"hash": <txid>, "index": <txix>}
+        self.inputs: list[dict[str, str]] = []
 
-        # address
+        # bech32 address of the person who will receive the remaining ADA
         self.change = None
 
         # pubkeyhash
         self.required_signer = None
 
-        self.outputs = []
+        self.outputs: list[dict] = []
 
-        self.mint_scripts = []
-        self.mint_value = {}
+        self.mint_scripts: list[dict[str, str]] = []
+        self.mint_value = None
 
         self.metadata_path = None
 
-    def add_collateral(self, txhash, txix):
+    def set_collateral(self, txhash: str, txix: str):
+        if self.collateral is not None:
+            logging.warning(
+                "Replacing existing collateral "
+                f"{self.collateral['hash']}#{self.collateral['index']} "
+                f"with {txhash}#{txix}"
+            )
+
         self.collateral = {
             "hash": txhash,
             "index": txix
         }
 
-    def add_input(self, txhash, txix):
+    def add_input(self, txhash: str, txix: str):
         self.inputs.append({
+            "type": "no-script",
             "hash": txhash,
             "index": txix
         })
 
-    def add_script_input(self, txhash, txix, script_path, redeemer_path, datum_path):
+    def add_script_input(self, txhash: str, txix: str, script_path: str,
+                         redeemer_path: str, datum_path: str):
         self.inputs.append({
+            "type": "script",
             "hash": txhash,
             "index": txix,
             "script": script_path,
@@ -57,42 +61,45 @@ class TxBody(object):
             "datum": datum_path
         })
 
-    def add_change(self, addr):
+    def set_change(self, addr: str):
+        if self.change is not None:
+            logging.warning(
+                f"Replacing existing change address {self.change} {addr}")
+
         self.change = addr
 
-    def add_required_signer(self, pubkeyhash):
+    def set_required_signer(self, pubkeyhash: str):
         self.required_signer = pubkeyhash
 
-    def add_output(self, receiver, value):
+    def add_output(self, receiver: str, value: Value):
         self.outputs.append({
+            "type": "no-script",
             "address": receiver,
-            "value": value.get(),
-            "datum": None
+            "value": value.get()
         })
 
-    def add_output_with_datum(self, receiver, value, datum_path):
+    def add_output_with_datum(self, receiver: str, value: Value, datum_path: str):
         self.outputs.append({
+            "type": "script",
             "address": receiver,
             "value": value.get(),
             "datum": datum_path
         })
 
-    def add_mint_script(self, script_path, redeemer_path):
+    def add_mint_script(self, script_path: str, redeemer_path: str):
         self.mint_scripts.append({
             "script": script_path,
             "redeemer": redeemer_path
         })
 
-    def set_mint_value(self, value):
+    def set_mint_value(self, value: Value):
         self.mint_value = value.get()
 
-    def set_metadata(self, metadata_path):
+    def set_metadata(self, metadata_path: str):
         self.metadata_path = metadata_path
 
-    def get(self):
+    def get(self) -> dict[str, Any]:
         return {
-            "era": self.era,
-            "magic": self.magic,
             "collateral": self.collateral,
             "inputs": self.inputs,
             "change": self.change,
@@ -102,14 +109,35 @@ class TxBody(object):
             "mint_value": self.mint_value
         }
 
-    def cli(self, protocol_parameters_file, out_file):
+    def cli(self, magic: str, protocol_parameters_file: str, out_file: str,
+            cddl_mode: bool = False) -> str:
+        # Make sure our cli is valid (at least the validations
+        # we can do without a node)
+
+        # See if we have the minimum arguments (input, output, change)
+        if self.inputs == [] or self.outputs == [] or self.change is None:
+            raise ValueError(
+                "Transaction missing one of the required parameters: input, output or change")
+
+        # If we have any script, make sure we also have a collateral
+        if self.collateral is None and \
+                (any([_input["type"] == "script" for _input in self.inputs]) or
+                 any([output["type"] == "script" for output in self.outputs])):
+            raise ValueError(
+                "Transaction has scripts, but collateral is not set!")
+
+        # If we have mint scripts but no mint value should fail
+        if self.mint_scripts != [] and self.mint_value is None:
+            raise ValueError("Mint script added, but no mint value defined!")
+
         inputs = []
         for _input in self.inputs:
             inputs.append(f"--tx-in {_input['hash']}#{_input['index']} \\")
             if "script" in _input and "redeemer" in _input and "datum" in _input:
-                inputs.append(f"--tx-in-script-file {_input['script']} \\")
-                inputs.append(f"--tx-in-redeemer-file {_input['redeemer']} \\")
-                inputs.append(f"--tx-in-datum-file {_input['datum']} \\")
+                inputs.append(f"    --tx-in-script-file {_input['script']} \\")
+                inputs.append(
+                    f"    --tx-in-redeemer-file {_input['redeemer']} \\")
+                inputs.append(f"    --tx-in-datum-file {_input['datum']} \\")
 
         outputs = []
         for output in self.outputs:
@@ -119,24 +147,25 @@ class TxBody(object):
 
             parsed_tokens = " + ".join(tokens)
 
-            outputs.append(f"--tx-out \"{output['address']} {parsed_tokens}\" \\")
-            if output["datum"] is not None:
+            outputs.append(
+                f"--tx-out \"{output['address']} {parsed_tokens}\" \\")
+            if "datum" in output:
                 outputs.append(
-                    f"--tx-out-datum-embed-file {output['datum']} \\")
-
-        tokens = []
-        for asset, amount in self.mint_value.items():
-            tokens.append(f"{amount} {asset}")
+                    f"    --tx-out-datum-embed-file {output['datum']} \\")
 
         mints = []
-        if len(tokens) > 0:
+        if self.mint_value is not None:
+            tokens = []
+            for asset, amount in self.mint_value.items():
+                tokens.append(f"{amount} {asset}")
+
             parsed_tokens = " + ".join(tokens)
 
             mints.append(f"--mint=\"{parsed_tokens}\" \\")
 
             for item in self.mint_scripts:
-                mints.append(f"--mint-script-file {item['script']} \\")
-                mints.append(f"--mint-redeemer-file {item['redeemer']} \\")
+                mints.append(f"    --mint-script-file {item['script']} \\")
+                mints.append(f"    --mint-redeemer-file {item['redeemer']} \\")
 
         required_signer_hash = []
         if self.required_signer:
@@ -152,10 +181,9 @@ class TxBody(object):
         if self.metadata_path:
             metadata.append(f"--metadata-json-file {self.metadata_path} \\")
 
-        cli_body = "\n".join(map(lambda x: "  " + x, [
-            f"--cddl-format \\",
-            f"--{self.era}-era \\",
-            f"--testnet-magic {self.magic} \\",
+        cli_body = "\n".join(map(lambda x: "    " + x, [
+            f"--cddl-format \\" if cddl_mode else f"--cli-format \\",
+            f"--testnet-magic {magic} \\",
             *inputs,
             *required_signer_hash,
             *collateral,
@@ -172,89 +200,27 @@ class TxBody(object):
             cli_body
         ])
 
-    def sign(self, tx_body, skeys, out):
-        parsed_keys = []
-        for skey in skeys:
-            parsed_keys.append(f"--signing-key-file {skey} \\")
-
-        cli = "\n".join([
-            "cardano-cli transaction sign \\",
-            f"--tx-body-file {tx_body} \\",
-            *parsed_keys,
-            f"--testnet-magic {self.magic} \\",
-            f"--out-file {out}"
-        ])
-
-        return cli
-
-    def assemble(self, tx_body, tx_witness, out):
-        cli = "\n".join([
-            "cardano-cli transaction assemble \\",
-            f"--tx-body-file {tx_body} \\",
-            f"--witness-file {tx_witness} \\",
-            f"--out-file {out}",
-        ])
-
-        return cli
-
-    def submit(self, tx_sig):
-        cli = "\n".join([
-            "cardano-cli transaction submit \\",
-            f"--tx-file {tx_sig} \\",
-            f"--testnet-magic {self.magic}",
-        ])
-
-        return cli
-
     @staticmethod
-    def calculate_min_utxo(
-        receiver: str, value: Value, protocol_params_path: str,
-        datum_path: str | None = None
-    ) -> str:
-        output = []
-
-        tokens = []
-        for asset, amount in value.value.items():
-            tokens.append(f"{amount} {asset}")
-
-        parsed_tokens = " + ".join(tokens)
-
-        output.append(f"--tx-out \"{receiver} {parsed_tokens}\" \\")
-        if datum_path is not None:
-            output.append(
-                f"--tx-out-datum-embed-file {datum_path} \\")
-
-        cli = "\n".join([
-            "cardano-cli transaction calculate-min-required-utxo \\",
-            f"--protocol-params-file {protocol_params_path} \\",
-            *output,
-        ]).rstrip(" \\")
-
-        return cli
-
-    @staticmethod
-    def build(era: str, magic: str, inputs: List[TxOutRef], change_address: str,
-              outputs: List[Dict[str, str | Value]], metadata_path: str | None = None) -> TxBody:
+    def build(change_address: str, inputs: list[TxOutRef], outputs: list[dict[str, str | Value]],
+              metadata_path: str | None = None) -> TxBody:
         """
         Builds common shelley cardano transaction bodies given certain arguments.
 
         Args:
-            era: The era in which this transaction should be built. (e.g. alonzo)
-            magic: The current magic number. (e.g. 1097911063)
-            inputs: The list of TxOutRef UTxOs that will be used as inputs
             change_address: The address that will receive the change
+            inputs: The list of TxOutRef UTxOs that will be used as inputs
             outputs: The list of address, value of the receivers in the format {"address": <addr>, "value": <value>}
             metadata_path: The path for the metadata json file
 
         Returns:
             The corresponding TxBody
         """
-        body = TxBody(era, magic)
+        body = TxBody()
 
         for _input in inputs:
             body.add_input(_input.tx_id, _input.tx_ix)
 
-        body.add_change(change_address)
+        body.set_change(change_address)
 
         for output in outputs:
             address: str = output["address"]
--- tests/test_tx_body.py
+++ tests/test_tx_body.py
@@ -1,14 +1,266 @@
+import pytest
 from Plutupus import TxBody
 from Plutupus.Types.TxOutRef import TxOutRef
 from Plutupus.Types.Value import Value
 
 
+def test_build_methods():
+    body = TxBody()
+    body.set_collateral("hash", "index")
+
+    assert body.collateral == {
+        "hash": "hash",
+        "index": "index"
+    }
+
+    body.add_input("hash", "index")
+
+    assert body.inputs == [{
+        "type": "no-script",
+        "hash": "hash",
+        "index": "index",
+    }]
+
+    body.add_script_input("hash", "index", "script", "redeemer", "datum")
+
+    assert body.inputs == [{
+        "type": "no-script",
+        "hash": "hash",
+        "index": "index",
+    }, {
+        "type": "script",
+        "hash": "hash",
+        "index": "index",
+        "script": "script",
+        "redeemer": "redeemer",
+        "datum": "datum"
+    }]
+
+    body.set_change("addr")
+
+    assert body.change == "addr"
+
+    body.set_required_signer("pkh")
+
+    assert body.required_signer == "pkh"
+
+    body.add_output("receiver", Value.lovelace(2_000_000))
+
+    assert body.outputs == [{
+        "type": "no-script",
+        "address": "receiver",
+        "value": {
+            "lovelace": 2_000_000
+        },
+    }]
+
+    body.add_output_with_datum("receiver", Value.lovelace(3_000_000), "datum")
+
+    assert body.outputs == [{
+        "type": "no-script",
+        "address": "receiver",
+        "value": {
+            "lovelace": 2_000_000
+        },
+    }, {
+        "type": "script",
+        "address": "receiver",
+        "value": {
+            "lovelace": 3_000_000
+        },
+        "datum": "datum"
+    }]
+
+    body.add_mint_script("script", "redeemer")
+    assert body.mint_scripts == [{
+        "script": "script",
+        "redeemer": "redeemer"
+    }]
+
+    body.set_mint_value(Value.lovelace(2_000_000))
+    assert body.mint_value == {
+        "lovelace": 2_000_000
+    }
+
+    body.set_metadata("metadata")
+    assert body.metadata_path == "metadata"
+
+
+def test_get():
+    body = TxBody()
+
+    body.set_collateral("hash", "index")
+    body.add_input("hash", "index")
+    body.add_script_input("hash", "index", "script", "redeemer", "datum")
+    body.set_change("addr")
+    body.set_required_signer("pkh")
+    body.add_output("receiver", Value.lovelace(2_000_000))
+    body.add_output_with_datum("receiver", Value.lovelace(3_000_000), "datum")
+    body.add_mint_script("script", "redeemer")
+    body.set_mint_value(Value.lovelace(2_000_000))
+    body.set_metadata("metadata")
+
+    assert body.get() == {
+        "collateral": {
+            "hash": "hash",
+            "index": "index"
+        },
+        "inputs": [{
+            "type": "no-script",
+            "hash": "hash",
+            "index": "index",
+        }, {
+            "type": "script",
+            "hash": "hash",
+            "index": "index",
+            "script": "script",
+            "redeemer": "redeemer",
+            "datum": "datum"
+        }],
+        "change": "addr",
+        "required_signer": "pkh",
+        "outputs": [{
+            "type": "no-script",
+            "address": "receiver",
+            "value": {
+                "lovelace": 2_000_000
+            },
+        }, {
+            "type": "script",
+            "address": "receiver",
+            "value": {
+                "lovelace": 3_000_000
+            },
+            "datum": "datum"
+        }],
+        "mint_scripts": [{
+            "script": "script",
+            "redeemer": "redeemer"
+        }],
+        "mint_value": {
+            "lovelace": 2_000_000
+        }
+    }
+
+
+def test_cli():
+    # A body must have at the minimum: an input, an output and a change
+
+    # No input
+    body = TxBody()
+    body.add_output("receiver", Value.lovelace(2_000_000))
+    body.set_change("sender")
+
+    with pytest.raises(ValueError):
+        body.cli("42", "protocol_params", "out_file")
+
+    # No output
+    body = TxBody()
+    body.add_input("hash", "index")
+    body.set_change("sender")
+
+    with pytest.raises(ValueError):
+        body.cli("42", "protocol_params", "out_file")
+
+    # No change
+    body = TxBody()
+    body.add_input("hash", "index")
+    body.add_output("receiver", Value.lovelace(2_000_000))
+
+    with pytest.raises(ValueError):
+        body.cli("42", "protocol_params", "out_file")
+
+    # No collateral, but w/ script (should fail)
+    body = TxBody()
+    body.add_script_input("hash", "index", "script", "redeemer", "datum")
+    body.add_output("receiver", Value.lovelace(2_000_000))
+    body.set_change("sender")
+
+    with pytest.raises(ValueError):
+        body.cli("42", "protocol_params", "out_file")
+
+    # With mint script but without mint value
+    body = TxBody()
+    body.add_input("hash", "index")
+    body.add_output("receiver", Value.lovelace(2_000_000))
+    body.set_change("sender")
+    body.add_mint_script("script", "redeemer")
+
+    with pytest.raises(ValueError):
+        body.cli("42", "protocol_params", "out_file")
+
+    # Simplest working body
+    body = TxBody()
+    body.add_input("hash", "index")
+    body.add_output("receiver", Value.lovelace(2_000_000))
+    body.set_change("sender")
+
+    # cli format
+    assert body.cli("42", "protocol_params", "out_file") == "\n".join([
+        "cardano-cli transaction build \\",
+        "    --cli-format \\",
+        "    --testnet-magic 42 \\",
+        "    --tx-in hash#index \\",
+        "    --tx-out \"receiver 2000000 lovelace\" \\",
+        "    --change-address sender \\",
+        "    --protocol-params-file protocol_params \\",
+        "    --out-file out_file"
+    ])
+
+    # cddl format
+    assert body.cli("42", "protocol_params", "out_file", cddl_mode=True) == "\n".join([
+        "cardano-cli transaction build \\",
+        "    --cddl-format \\",
+        "    --testnet-magic 42 \\",
+        "    --tx-in hash#index \\",
+        "    --tx-out \"receiver 2000000 lovelace\" \\",
+        "    --change-address sender \\",
+        "    --protocol-params-file protocol_params \\",
+        "    --out-file out_file"
+    ])
+
+    # Body with everything combined
+
+    body = TxBody()
+    body.set_collateral("hash", "index")
+    body.add_input("hash1", "index1")
+    body.add_script_input("hash2", "index2", "script", "redeemer", "datum")
+    body.set_change("addr")
+    body.set_required_signer("pkh")
+    body.add_output("receiver", Value.lovelace(2_000_000))
+    body.add_output_with_datum("receiver", Value.lovelace(3_000_000), "datum")
+    body.add_mint_script("script", "redeemer")
+    body.set_mint_value(Value.lovelace(2_000_000))
+    body.set_metadata("metadata")
+
+    assert body.cli("42", "protocol_params", "out_file") == "\n".join([
+        "cardano-cli transaction build \\",
+        "    --cli-format \\",
+        "    --testnet-magic 42 \\",
+        "    --tx-in hash1#index1 \\",
+        "    --tx-in hash2#index2 \\",
+        "        --tx-in-script-file script \\",
+        "        --tx-in-redeemer-file redeemer \\",
+        "        --tx-in-datum-file datum \\",
+        "    --required-signer-hash pkh \\",
+        "    --tx-in-collateral hash#index \\",
+        "    --tx-out \"receiver 2000000 lovelace\" \\",
+        "    --tx-out \"receiver 3000000 lovelace\" \\",
+        "        --tx-out-datum-embed-file datum \\",
+        "    --change-address addr \\",
+        "    --mint=\"2000000 lovelace\" \\",
+        "        --mint-script-file script \\",
+        "        --mint-redeemer-file redeemer \\",
+        "    --metadata-json-file metadata \\",
+        "    --protocol-params-file protocol_params \\",
+        "    --out-file out_file"
+    ])
+
+
 def test_build():
     body = TxBody.build(
-        "alonzo",
-        "magic_number",
-        [TxOutRef("tx_hash_1", "tx_ix_1"), TxOutRef("tx_hash_2", "tx_ix_2")],
         "sender_address",
+        [TxOutRef("tx_hash_1", "tx_ix_1"), TxOutRef("tx_hash_2", "tx_ix_2")],
         [
             {"address": "receiver_address_1",
                 "value": Value.lovelace(5_000_000)},
@@ -18,56 +270,30 @@ def test_build():
         "path/to/metadata"
     )
 
-    assert body.era == "alonzo"
-    assert body.magic == "magic_number"
     assert body.inputs == [{
+        "type": "no-script",
         "hash": "tx_hash_1",
-        "index": "tx_ix_1"
+        "index": "tx_ix_1",
     }, {
+        "type": "no-script",
         "hash": "tx_hash_2",
-        "index": "tx_ix_2"
+        "index": "tx_ix_2",
     }]
     assert body.change == "sender_address"
     assert body.outputs == [
         {
+            "type": "no-script",
             "address": "receiver_address_1",
             "value": {
                 "lovelace": 5_000_000
             },
-            "datum": None
         },
         {
+            "type": "no-script",
             "address": "receiver_address_2",
             "value": {
                 "lovelace": 7_000_000
             },
-            "datum": None
         }
     ]
     assert body.metadata_path == "path/to/metadata"
-
-
-def test_calculate_min_utxo():
-    receiver = "addr_testabc"
-
-    value = Value()
-
-    value.add_token_amount("lovelace", 13_000_000)
-    value.add_token_amount("abc123.444444", 5_000_000)
-    value.add_token_amount("def456.555555", 3_000_000)
-    value.add_token_amount("def456.666666", 17_000_000)
-
-    datum_path = "/path/to/datum"
-
-    # cardano-cli transaction calculate-min-required-utxo \
-    # --protocol-params-file path/to/protocol.json \
-    # --tx-out "addr_testabc 13000000 lovelace + 5000000 abc123.444444 + 3000000 def456.555555 + 17000000 def456.666666" \
-    # --tx-out-datum-embed-file /path/to/datum \
-
-    assert TxBody.calculate_min_utxo(
-        receiver, value, "path/to/protocol.json", datum_path) == "\n".join([
-            "cardano-cli transaction calculate-min-required-utxo \\",
-            "--protocol-params-file path/to/protocol.json \\",
-            '--tx-out "addr_testabc 13000000 lovelace + 5000000 abc123.444444 + 3000000 def456.555555 + 17000000 def456.666666" \\',
-            "--tx-out-datum-embed-file /path/to/datum"
-        ])
